145e27da6da19262980693ca1acec0be29a1684b
go-lauren <go.lauren@icloud.com>
2022-08-20 15:35:43 -0700

    move to classes

---
+++ minesweeper.py
@@ -0,0 +1,156 @@
+import sys
+import time
+
+#  constants
+MINE = 9
+UNKNOWN = -1
+
+
+class Array:
+    colors = {
+        MINE: "\033[0;30;41m",  # red background
+        1: "\033[34m",  # blue
+        2: "\033[1;32;48m",
+        3: "\033[31m",  # red
+        4: "\033[1;31;48m",  # red
+        5: "\033[1;31;48m",  # red
+        6: "\033[1;30;48m",
+        7: "\033[1;33;48m",
+        8: "\033[1;33;48m",
+        0: "\033[30m",  # black
+        UNKNOWN: "\033[0;30;47m",  # bold
+        "END": "\033[0m",
+    }
+
+    def __init__(self, m, n, val):
+        self.m = m
+        self.n = n
+        self.arr = [[val for _ in range(n)] for _ in range(m)]
+
+    def __getitem__(self, item):
+        return self.arr[item]
+
+    def print(self):
+        def color(item):
+            return self.colors[item] + "{:3}".format(item) + self.colors["END"]
+
+        def gray_block():
+            return self.colors[UNKNOWN] + " " * 3 + self.colors["END"]
+
+        print(gray_block() * (self.m + 2))
+        print(
+            ("\n").join(
+                [
+                    (
+                        gray_block()
+                        + "".join([color(item) for item in row])
+                        + gray_block()
+                    )
+                    for row in self.arr
+                ]
+            )
+        )
+        print(gray_block() * (self.m + 2))
+
+    def unprint(self):
+        for _ in range(self.m + 2):
+            sys.stdout.write("\x1b[1A")
+            sys.stdout.write("\x1b[2K")
+
+    def neighbors(self, i, j, radius=1):
+        lst = []
+        for ii in range(max(i - radius, 0), min(i + radius + 1, self.m)):
+            for jj in range(max(j - radius, 0), min(j + radius + 1, self.n)):
+                if ii == i and jj == j:
+                    continue
+                lst.append((ii, jj))
+        return lst
+
+
+class Field(Array):
+    def __init__(self, m, n):
+        super().__init__(m, n, 0)
+        self.field = self.arr
+
+    def set_mines(self, mine_locations):
+        for mine in mine_locations:
+            i = mine[0]
+            j = mine[1]
+            self.field[i][j] = MINE
+            for ni, nj in self.neighbors(i, j):
+                if self.field[ni][nj] == 9:
+                    continue
+                self.field[ni][nj] += 1
+        return self.field
+
+
+class Solution(Array):
+    def __init__(self, m, n):
+        super().__init__(m, n, -1)
+        self.solution = self.arr
+
+    def set_flag(self, i, j):
+        self.solution[i][j] = MINE
+
+    def next_step(self):
+        reveal_all = []
+        flags = []
+        for i in range(self.m):
+            for j in range(self.n):
+                if self.solution[i][j] == UNKNOWN:
+                    continue
+                elif self.solution[i][j] == MINE:
+                    continue
+                mines = 0
+                unknowns = []
+                for ni, nj in self.neighbors(i, j):
+                    if self.solution[ni][nj] == MINE:
+                        mines += 1
+                    elif self.solution[ni][nj] == UNKNOWN:
+                        unknowns.append((ni, nj))
+                if mines == self.solution[i][j]:
+                    if len(unknowns) > 0:
+                        reveal_all.append((i, j))
+                elif mines + len(unknowns) == self.solution[i][j]:
+                    flags += unknowns
+        return reveal_all, flags
+
+
+class Game:
+    def __init__(self, field, solution):
+        assert isinstance(field, Field)
+        assert isinstance(solution, Solution)
+        self.field = field
+        self.solution = solution
+
+    def reveal(self, i, j):
+        if self.field.field[i][j] == MINE:
+            print("You lost...")
+
+        self.solution[i][j] = self.field[i][j]
+
+        if self.solution[i][j] == 0:
+            for ni, nj in self.solution.neighbors(i, j):
+                if self.solution[ni][nj] != UNKNOWN:
+                    continue
+                self.reveal(ni, nj)
+
+    def solve(self, i=0, j=0):
+        print("Starting guess at ({0},{1})...".format(i, j))
+        self.reveal(i, j)
+        self.solution.print()
+        while True:
+            reveal_all, flags = self.solution.next_step()
+            if not len(reveal_all) and not len(flags):
+                break
+            self.solution.unprint()
+            for i, j in flags:
+                self.solution.set_flag(i, j)
+            print("Flagging field...")
+            for i, j in reveal_all:
+                for ni, nj in self.solution.neighbors(i, j):
+                    if self.solution[ni][nj] == UNKNOWN:
+                        self.reveal(ni, nj)
+            print("Revealing tiles...")
+            self.solution.print()
+            time.sleep(0.5)
---
+++ tests.py
@@ -0,0 +1,43 @@
+from minesweeper import *
+
+M = 9
+N = 9
+mines1 = [
+    (0, 4),
+    (0, 5),
+    (0, 7),
+    (2, 4),
+    (5, 0),
+    (5, 7),
+    (6, 5),
+    (6, 6),
+    (7, 6),
+    (8, 8),
+]
+
+mines2 = [
+    (0, 0),
+    (1, 4),
+    (2, 3),
+    (2, 8),
+    (3, 2),
+    (5, 2),
+    (6, 2),
+    (6, 8),
+    (7, 3),
+    (8, 5),
+]
+
+
+def run_test(mines, m, n, initial_guess=(0, 0)):
+    field = Field(m, n)
+    field.set_mines(mines)
+    print("Solution...")
+    field.print()
+    sol = Solution(m, n)
+    game = Game(field, sol)
+    game.solve(initial_guess[0], initial_guess[1])
+
+
+run_test(mines1, M, N, (0, 0))
+run_test(mines2, M, N, (4, 4))
